1a399380757d0f1b2c7e68f695cf7bfa85192db4
Administrator <Administrator@MSDN-SPECIAL>
2019-12-10 13:45:41 +0900

    공지사항 리스트 작업중

--- lifrary/src/main/java/ksmart/pentagon/controller/BoardController.java
+++ lifrary/src/main/java/ksmart/pentagon/controller/BoardController.java
@@ -7,7 +7,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class BoardController {
 	
 	@GetMapping("noticeSearch")
-	public String noticeInsert() {
+	public String noticeSearch() {
 		return "adminpage/board/noticeSearch";
+	}
+	@GetMapping("noticeSearchList")
+	public String noticeSearchList() {
+		return "adminpage/board/noticeSearchList";
 	}
 }
