c873fbdae09930a18931e06d8cf3b25f3ba1d72f
Administrator <Administrator@MSDN-SPECIAL>
2019-12-11 11:36:39 +0900

    [pentagon 한우리] 제목 : 관리자페이지-회원관리작성 예: 회원관리(회원등록), 사서관리(사서등록,수정,삭제)
    내용: 회원관리-회원전체검색리스트(userSearchList.html)
    		  -회원수정(adminUserUpdate.html)
    		  -회원상세페이지(adminUserDetail.html)
    	 사서관리-관리자 사서등록(librarianInsert.html)
    	 	  -관리자 사서검색리스트(librarianSearchList.html)
    
    파일:
    자세한 내용은 파일내 주석처리
    간단한 요약내용 임의로 파일명뒤 작성
    
    생성파일&수정파일
    AdminController.java
    AdminMapper.java
    AdminService.java
    ksmart.pentagon.vo - LibrarianLevel.java
    				   - User.java
    AdminMapper.xml
    static/admin/js/admin_user_tables.js
    templates/templates/librarian - librarianInsert.html
    							  - librarianSearchList.html
    templates/templates/userManagement - adminUserDetail.html
    								   - adminUserUpdate.html
    								   - userLevelInsert.html
    								   - userSearchList.html
    sideNav.html 에서 경로 수정 했슴

--- lifrary/src/main/java/ksmart/pentagon/user/AdminService.java
+++ lifrary/src/main/java/ksmart/pentagon/user/AdminService.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import ksmart.pentagon.vo.LibrarianLevel;
+import ksmart.pentagon.vo.User;
 import ksmart.pentagon.vo.UserLevel;
 
 @Service
@@ -18,4 +20,43 @@ public class AdminService {
 		
 		return adminMapper.getUserLevel();
 	}
+	
+	//유저 회원 전체 가져오기 
+	public List<User> getUserList(){
+		System.out.println("getUserList 서비스진입");
+		System.out.println(adminMapper.getUserList());
+		
+		return adminMapper.getUserList();
+	}
+	
+	//유저 회원 리스트 검색 가져오기
+	public List<User> getUserSearch(){
+		System.out.println("getUserSearch 서비스진입");
+		System.out.println(adminMapper.getUserSearch());
+		
+		return adminMapper.getUserSearch();
+	}
+	
+	//유저 회원 수정 처리 
+	public int adminUserUpdate(User user) {
+		System.out.println("adminUserUpdate 서비스진입");
+		
+		return adminMapper.adminUserUpdate(user);
+	}
+	
+	//유저 회원 수정 화면 불러오기
+	public User getAdminUserUpdate(String uId) {
+		System.out.println("adminUserUpdateId 서비스진입");
+		
+		return adminMapper.getAdminUserUpdate(uId);
+	}
+	
+	//관리자가 사서 등록하기 
+	public int librarianInsert(LibrarianLevel librarianLevel) {
+		System.out.println("librarianInsert 서비스진입");
+		int result = adminMapper.librarianInsert(librarianLevel);
+		System.out.println(result + "==> libraryMapper.librarianInsert(librarianLevel) ");
+			
+		return result;
+	} 
 }
