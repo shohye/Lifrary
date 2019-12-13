b6f4f965c39c5454977e183055a741edaeab084e
Administrator <Administrator@smart20121>
2019-12-13 17:18:37 +0900

    [pentagon 최지혜] 북다이어리관리 내용: 	북캘린더 레이아웃 구현, 	마이페이지 사이드 메뉴 생성
    파일:
    생성파일
    
    fragments/library/myPageLeftMenu.html 마이페이지 사이드메뉴
    librarypage/bookReport/myBookReportSearchList.html 내 독후감 리스트
    
    수정파일
    
    templates/librarypage/calender/myCalender.html
    
    삭제파일

--- lifrary/src/main/java/ksmart/pentagon/mypage/BookReportController.java
+++ lifrary/src/main/java/ksmart/pentagon/mypage/BookReportController.java
@@ -14,49 +14,49 @@ public class BookReportController {
 	
 	/**
 	 * @brief 독후감 공유 리스트
-	 * @return /librarypage/book/bookReportSearchList
+	 * @return /librarypage/bookReport/bookReportSearchList
 	 * @author 최지혜
 	 */
 	@GetMapping("/lifrary/bookReportSearchList")
 	public String bookReportSearchList() {
 		
-		return "/librarypage/book/bookReportSearchList";
+		return "/librarypage/bookReport/bookReportSearchList";
 		
 	}
 	
 	/**
 	 * @brief 마이페이지 내 독후감 리스트
-	 * @return /librarypage/book/myBookReportSearchList
+	 * @return /librarypage/bookReport/myBookReportSearchList
 	 * @author 최지혜
 	 */
 	@GetMapping("/lifrary/myBookReportSearchList")
 	public String myBookReportSearchList() {
 		
-		return "/librarypage/book/myBookReportSearchList";
+		return "/librarypage/bookReport/myBookReportSearchList";
 		
 	}
 	
 	/**
 	 * @brief 독후감 등록 화면
-	 * @return /librarypage/book/bookReportInsert
+	 * @return /librarypage/bookReport/bookReportInsert
 	 * @author 최지혜
 	 */
 	@GetMapping("/lifrary/bookReportInsert")
 	public String bookReportInsert() {
 		
-		return "/librarypage/book/bookReportInsert";
+		return "/librarypage/bookReport/bookReportInsert";
 	
 	}
 	
 	/**
 	 * @brief 독후감 상세 
-	 * @return /librarypage/book/bookReportDetail
+	 * @return /librarypage/bookReport/bookReportDetail
 	 * @author 최지혜
 	 */
 	@GetMapping("/lifrary/bookReportDetail")
 	public String bookReportDetail() {
 		
-		return "/librarypage/book/bookReportDetail";
+		return "/librarypage/bookReport/bookReportDetail";
 	
 	}
 	
