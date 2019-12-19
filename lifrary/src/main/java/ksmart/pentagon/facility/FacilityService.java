f1eb4bbca3354dba22aee16a57b110b1877140fe
SangHyoub <tkdguq93@gmail.com>
2019-12-19 16:45:38 +0900

    [pentagon 김상협] 제목 : 공공시설 관련 기능 / 페이지 구현중
    내용:
    공공시설 관련 기능 / 페이지 구현중
    공공시설 리스트 출력 완료.
    라디오버튼으로 공공시설 누르면 리스트 재 출력
    
    
    파일:
    
    생성파일
    lifrary/src/main/java/ksmart/pentagon/vo/Facility.java
    lifrary/src/main/resources/templates/adminpage/facility/facilityDetail.html
    lifrary/src/main/resources/templates/adminpage/facility/facilityInsert.html
    lifrary/src/main/resources/templates/adminpage/facility/facilityList.html
    lifrary/src/main/resources/templates/adminpage/facility/facilityUpdate.html
    공공시설 관련 페이지 생성 / vo 생성
    
    
    수정파일
    FacilityController.java
    FacilityMapper.java
    FacilityMapper.xml
    FacilityService.java
    pom.xml  -> ajax를 사용하기 위해 maven 추가
    sideNav.html -> 공공시설 경로 추가
    
    
    삭제파일
    
    
    
    
    ajax로 좌석 테이블 불러오기

--- lifrary/src/main/java/ksmart/pentagon/facility/FacilityService.java
+++ lifrary/src/main/java/ksmart/pentagon/facility/FacilityService.java
@@ -1,7 +1,11 @@
 package ksmart.pentagon.facility;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import ksmart.pentagon.vo.Facility;
 
 /*
  * @file   facilityService.java 
@@ -14,4 +18,14 @@ import org.springframework.stereotype.Service;
 public class FacilityService {
 
 	@Autowired private FacilityMapper facilityMapper;
+	
+	//시설종류, 시설코드를 받아 리스트 받아오는 메서드
+	public List<Facility> getFacilityList(String fKinds, String libNum){
+		return facilityMapper.getFacilityList(fKinds, libNum);
+	}
+	
+	//선택한 시설의 코드를 이용하여 , 해당 시설의 상세정보 리스트 보여주기
+		public Facility getFacility(String fCode, String libNum) {
+			return facilityMapper.getFacility(fCode, libNum);
+		}
 }
